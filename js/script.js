bdf0d2ba5ae61bf3c580815f5086c5c8b9cf7bd7
maiTalavera <maiantonella.t@gmail.com>
2022-11-01 21:44:29 -0300

    tarea 1 js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//let nombreingresado=prompt ("ingrese su nombre")
+let nombre =  "maira"
+console.log(nombre);
+let apellido = "talavera"
+console.log(apellido);
+let edad = 30;
+console.log(edad);
+let tienesmascota = "no"
+console.log(tienesmascota);
+
